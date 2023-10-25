44763f7f5db46a65ea5f3f3abe5da14bde1614c6
Askar Oralkhan <askar4ik123@gmail.com>
2023-10-25 20:13:56 +0600

    chore: added server side validation

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,13 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
     public class Category
     {
-        [Key] // data annotation 
+        [Key] // data annotation, primary key 
         public int Id { get; set; }
-        [Required]
+        [Required] // cannot be null
+        [MaxLength(30)] // the length of the name of category must be less than 30
+        [DisplayName("Category Name")] 
         public string Name { get; set; }
+        [DisplayName("Display Order")] // how this will be displayed in view
+        [Range(1, 100)] // the value of display order must be between 1 and 100
         public int DisplayOrder { get; set; }
     }
 }
