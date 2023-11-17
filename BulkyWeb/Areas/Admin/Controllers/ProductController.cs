9b36e952cd1ad98df276a06399c2af44360790c8
Askar Oralkhan <askar4ik123@gmail.com>
2023-11-17 22:39:02 +0600

    chore: created api which return json file with product table data

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -132,5 +132,13 @@ namespace BulkyWeb.Areas.Admin.Controllers
             TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll() {
+            List<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = productList });
+        }
+        #endregion
     }
 }
