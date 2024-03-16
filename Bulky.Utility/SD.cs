ca20984f1d95b0f8536c93613c73fb54cfae5876
Askar Oralkhan <askar4ik123@gmail.com>
2024-03-17 00:37:57 +0500

    chore: added roles to the database

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -8,5 +8,9 @@ namespace Bulky.Utility
 {
     public static class SD
     {
+        public const string Role_Costomer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
     }
 }
