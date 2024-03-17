277863908f827d429c5acdb1b82aac8225b22bca
Askar Oralkhan <askar4ik123@gmail.com>
2024-03-17 21:31:17 +0500

    chore: added helper methods in order header repository

---
+++ Bulky.DataAccess/Migrations/20240317162023_addSessionIdToOrderHeader.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSessionIdToOrderHeader : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ShippingDate",
+                table: "OrderHeaders",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PaymentDate",
+                table: "OrderHeaders",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "OrderHeaders",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SessionId",
+                table: "OrderHeaders",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SessionId",
+                table: "OrderHeaders");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ShippingDate",
+                table: "OrderHeaders",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PaymentDate",
+                table: "OrderHeaders",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "OrderHeaders",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+    }
+}
--- Bulky.DataAccess/Repository/OrderHeaderRepository.cs
+++ Bulky.DataAccess/Repository/OrderHeaderRepository.cs
@@ -16,5 +16,32 @@ namespace Bulky.DataAccess.Repository
         {
             _dbContext.OrderHeaders.Update(orderHeader);
         }
-    }
+
+		public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+		{
+			var orderFromDb = _dbContext.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if(orderFromDb != null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if(!string.IsNullOrWhiteSpace(paymentStatus))
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+		}
+
+		public void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId)
+		{
+			var orderFromDb = _dbContext.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                orderFromDb.SessionId = sessionId;
+            }
+			if (!string.IsNullOrEmpty(paymentIntentId))
+			{
+				orderFromDb.PaymentIntentId = paymentIntentId;
+                orderFromDb.PaymentDate = DateTime.Now;
+			}
+		}
+	}
 }
