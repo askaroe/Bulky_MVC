f72a6962758e305c817016bb78a9b898e00ef30e
Askar Oralkhan <askar4ik123@gmail.com>
2024-03-18 01:44:05 +0500

    fix: fixed error

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -179,7 +179,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
 				Session session = service.Get(orderHeader.SessionId);
 				if(session.PaymentStatus.ToLower() == "paid")
 				{
-					_unitOfWork.OrderHeader.UpdateStripePaymentID(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+					_unitOfWork.OrderHeader.UpdateStripePaymentID(id, session.Id, session.PaymentIntentId);
 					_unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
 					_unitOfWork.Save();
 				}
