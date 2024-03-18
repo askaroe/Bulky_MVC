61a37dc72a2a415583bcfdcad27dcd61fd1cde36
Askar Oralkhan <askar4ik123@gmail.com>
2024-03-19 02:04:23 +0500

    chore: added sessions to dispay items in shopping cart on nav bar

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Bulky.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Security.Claims;
@@ -53,15 +55,17 @@ namespace BulkyWeb.Areas.Customer.Controllers
                 //shopping cart is already exists 
                 cartFromDb.Count += shoppingCart.Count;
                 _unitOfWork.ShoppingCart.Update(cartFromDb);
+                _unitOfWork.Save();
             }
             else
             {
                 //adding cart to the repository
                 _unitOfWork.ShoppingCart.Add(shoppingCart);
+                _unitOfWork.Save();
+                HttpContext.Session.SetInt32(SD.SessionCart,
+                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
             }
             TempData["success"] = "Cart updated successfully";
-            
-            _unitOfWork.Save();
             return RedirectToAction(nameof(Index));
         }
 
