908211ebae09d8a44ace43e2a61e158d45425960
Askar Oralkhan <askar4ik123@gmail.com>
2024-03-19 03:57:07 +0500

    chore & fix: updated facebook registration  and login pages and fixed new user registration bug

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -19,7 +19,7 @@ namespace Bulky.Models
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
-        public int CompanyId { get; set; }
+        public int? CompanyId { get; set; }
         [ForeignKey("CompanyId")]
         [ValidateNever]
         public Company Company { get; set; }
